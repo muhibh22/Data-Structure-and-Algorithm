0fb17e3a12df9fb5049dda55d3315b446c340095
Md. Mohibul Hasan <111929761+muhibh22@users.noreply.github.com>
2023-08-06 11:15:27 +0600

    Add files via upload

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,7 +1,7 @@
+import java.util.List;
+
 public class SinglyLinkedList{
     private ListNode head;
-
-
     private static class ListNode{
         private int data;
         private ListNode next;
@@ -10,36 +10,161 @@ public class SinglyLinkedList{
             this.next=null;
         }
     }
+
     public void display(){
-        ListNode current= head;
-        while(current !=null){
-            System.out.print(current.data + "-->");
+        ListNode current=head;
+        while(current!=null){
+            System.out.print(current.data+ "-->");
             current=current.next;
         }
         System.out.println("null");
     }
-    public boolean search(int key){
+
+    public void insertAtTheBeginning(int data){
+        ListNode current = new ListNode(data);
+        current.next =head;
+        head= current;
+    }
+
+    public void insertEnd(int data){
+        ListNode newNode = new ListNode(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
         ListNode current = head;
+        while(null!=current.next){
+            current= current.next;
+        }
+        current.next=newNode;
+    }
+
+    public void insertPosition(int data, int position){
+        ListNode newNode=new ListNode(data);
+        if(position==1){
+            newNode.next=head;
+            head=newNode;
+        }
+        else{
+            ListNode previous=head;
+            int count=1;
+            while(count<position-1){
+                previous=previous.next;
+                count++;
+            }
+            ListNode current= previous.next; 
+            newNode.next=current;
+            previous.next=newNode;
+        }
+    }
+
+    public void reverse(){
+        ListNode current= head;
+        ListNode Next= null;
+        ListNode previous= null;
         while(current!=null){
-            if(current.data==key){
+            Next=current.next;
+            current.next=previous;
+            previous=current;
+            current=Next;
+        }
+        head=previous;
+    } 
+
+
+    public void insert(int data){
+        ListNode newNode= new ListNode(data);
+        ListNode current=head;
+        ListNode temp=null;
+        while(current!=null && current.data<newNode.data){
+            temp=current;
+            current=current.next;
+        }
+        newNode.next=current;
+        temp.next=newNode;
+    }
+
+    public boolean containLoop(){
+        ListNode fastptr=head;
+        ListNode slowptr=head;
+        while(fastptr!=null && fastptr.next!=null){
+            fastptr=fastptr.next.next;
+            slowptr=slowptr.next;
+            if(slowptr==fastptr){
                 return true;
             }
-            current=current.next;
         }
         return false;
     }
+    public ListNode startNodeInALoop(){
+        ListNode fastptr=head;
+        ListNode slowptr=head;
+        while(fastptr!=null && fastptr.next!=null){
+            fastptr=fastptr.next.next;
+            slowptr=slowptr.next;
+            if(slowptr==fastptr){
+                return getStartingNode(slowptr);
+            }
+        }
+        return null;
+    }
+
+    private ListNode getStartingNode(ListNode slowptr) {
+        ListNode temp=head;
+        while(temp!=slowptr){
+            temp=temp.next;
+            slowptr=slowptr.next;
+
+        }
+        return temp;
+    }
+
+
+    public void removeLoop(){
+        ListNode fastptr=head;
+        ListNode slowPtr = head;
+        while(fastptr!=null && fastptr.next!=null){
+            fastptr=fastptr.next.next;
+            slowPtr=slowPtr.next;
+            if(fastptr==slowPtr){
+                removeLoopNow(slowPtr);
+                return;
+            
+            }
+        }
+    }
+
+
+    private void removeLoopNow(ListNode slowPtr) {
+        ListNode temp=head;
+        while(temp.next!=slowPtr.next){
+            temp=temp.next;
+            slowPtr.next=slowPtr.next;
+        }
+        slowPtr.next=null;
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll= new SinglyLinkedList();
-        sll.head= new ListNode(10);
-        ListNode second= new ListNode(1);
-        ListNode third = new ListNode(3);
-        ListNode fourth = new ListNode (4);
+        sll.head=new ListNode(10);
+        ListNode second= new ListNode(20);
+        ListNode third= new ListNode(30);
+        ListNode fourth= new ListNode(40);
         sll.head.next=second;
-        second.next=third;
-        third.next= fourth;
-        /*now i will be printing the SinglyLinkedList */
-        sll.display();
-        System.out.println(sll.search(1));
+        second.next=third; 
+        third.next=fourth;
+        // fourth.next=second;
+        //sll.display();
+        // sll.insertAtTheBeginning(10);
+        // sll.display();
+        // sll.insertEnd(50);
+        // sll.display();
+        // sll.insertPosition(8,3);
+        // sll.display();
+        // sll.reverse();
+        // sll.display();
+        // System.out.println(sll.containLoop());
+        // System.out.println(sll.startNodeInALoop().data);
 
 
     }
