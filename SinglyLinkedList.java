283e95049d347adbc9d83bcc77eb373a1fcea043
Md. Mohibul Hasan <111929761+muhibh22@users.noreply.github.com>
2023-08-06 17:17:11 +0600

    Add files via upload

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -168,6 +168,26 @@ public class SinglyLinkedList{
         return dummy.next;
     }
 
+    public static ListNode sumListNode(ListNode a, ListNode b){
+        ListNode dummy= new ListNode(0);
+        ListNode tail=dummy;
+        int carry=0;
+        while(a!=null|| b!=null){
+
+            int x=(a!=null)? a.data:0;
+            int y=(b!=null)? b.data:0;
+            int sum=carry+x+y;
+            carry=sum/10;
+            tail.next=new ListNode(sum%10);
+            tail=tail.next;
+            if(a!=null)a=a.next;
+            if(b!=null)b=b.next;
+        }
+        if(carry>0){
+            tail.next=new ListNode(carry);
+        }
+        return dummy.next;
+    }
 
     public static void main(String[] args) {
         SinglyLinkedList sll= new SinglyLinkedList();
@@ -190,21 +210,30 @@ public class SinglyLinkedList{
         // sll.display();
         // System.out.println(sll.containLoop());
         // System.out.println(sll.startNodeInALoop().data);
-        SinglyLinkedList sll1= new SinglyLinkedList();
-        sll1.insertEnd(1);
+        // SinglyLinkedList sll1= new SinglyLinkedList();
+        // sll1.insertEnd(1);
+        // sll1.insertEnd(4);
+        // sll1.insertEnd(8);
+        // SinglyLinkedList sll2= new SinglyLinkedList();
+        // sll2.insertEnd(3);
+        // sll2.insertEnd(5);
+        // sll2.insertEnd(8);
+        // sll2.insertEnd(9);
+        // sll2.insertEnd(14);
+        // sll2.insertEnd(18);
+        // sll1.display();
+        // sll2.display();
+         SinglyLinkedList result=new SinglyLinkedList();
+        // result.head=merge(sll1.head,sll2.head);
+        // result.display(); 
+        SinglyLinkedList sll1=new SinglyLinkedList();
+        SinglyLinkedList sll2=new SinglyLinkedList();
+        sll1.insertEnd(7);
         sll1.insertEnd(4);
-        sll1.insertEnd(8);
-        SinglyLinkedList sll2= new SinglyLinkedList();
-        sll2.insertEnd(3);
+        sll1.insertEnd(9);
         sll2.insertEnd(5);
-        sll2.insertEnd(8);
-        sll2.insertEnd(9);
-        sll2.insertEnd(14);
-        sll2.insertEnd(18);
-        sll1.display();
-        sll2.display();
-        SinglyLinkedList result=new SinglyLinkedList();
-        result.head=merge(sll1.head,sll2.head);
+        sll2.insertEnd(6);
+        result.head=sumListNode(sll1.head, sll2.head);
         result.display();
 
     }
