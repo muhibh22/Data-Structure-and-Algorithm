61095e1c801235cffe7b7292b47d8cabba80a1e0
Md. Mohibul Hasan <111929761+muhibh22@users.noreply.github.com>
2023-08-07 01:28:43 +0600

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+public class ReverseString {
+    public static void main(String[] args) {
+        String str="ABCD";
+        System.out.println(reverse(str));
+
+
+    }
+
+    public static String reverse(String str){
+        Stack <Character> stack=new Stack<>();
+        char[] chars=str.toCharArray();
+        for(char c:chars){
+            stack.push(c);
+        }
+        for(int i=0; i<chars.length;i++){
+            chars[i]=stack.pop();
+        }
+        return new String (chars);
+    }
+}
