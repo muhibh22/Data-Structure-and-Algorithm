baae0a5ef5fb5f7aeb4fbcbbe0c5c44c9f056a91
Md. Mohibul Hasan <111929761+muhibh22@users.noreply.github.com>
2023-08-07 21:45:35 +0600

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,4 +1,6 @@
 import java.util.Stack;
+
+import javax.annotation.PostConstruct;
 public class BinaryTree {
     private TreeNode root;
 
@@ -50,11 +52,54 @@ public class BinaryTree {
         System.out.println();
 
     }
+
+    public void inOrderRecursive(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inOrderRecursive(root.left);
+        System.out.print(root.data +" ");
+        inOrderRecursive(root.right);
+    }
+
+    public void inOrderiterative(TreeNode root){
+        if (root==null){
+            return;
+        }
+        Stack <TreeNode> stack=new Stack<>();
+        TreeNode temp=root;
+        while(!stack.isEmpty()|| temp!=null){
+            if(temp!=null){
+                stack.push(temp);
+                temp=temp.left;
+            }
+            else{
+                temp=stack.pop();
+                System.out.print(temp.data+" ");
+                temp=temp.right;
+            }
+        }
+        System.out.println();
+    }
+
+    public void postOrderRecursive(TreeNode root){
+        if(root==null){
+            return;
+        }
+        postOrderRecursive(root.left);
+        postOrderRecursive(root.right);
+        System.out.print(root.data+ " ");
+    }
     public static void main(String[] args) {
         BinaryTree bt=new BinaryTree();
         bt.createBinaryTree();
         bt.preOrder(bt.root);
         System.out.println();
         bt.preOrderStack();
+        bt.inOrderRecursive(bt.root);
+        System.out.println();
+        bt.inOrderiterative(bt.root);
+        bt.postOrderRecursive(bt.root);
+        System.out.println();
     }
 }
